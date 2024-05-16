38bafb795c9e408374386961f115f223ebad9b0c
xuqian <854451736@qq.com>
2024-05-16 13:11:48 -0400

    update

--- blockchain/merkle_tree.py
+++ blockchain/merkle_tree.py
@@ -1,15 +1,14 @@
 import hashlib
+
 def merkle_root(transactions):
     if len(transactions) == 0:
         return '0'
     
-    # 如果交易数量不是偶数，将最后一个交易复制一份
-    if len(transactions) % 2 != 0:
-        transactions.append(transactions[-1])
-        
     def combine(hash_list):
         if len(hash_list) == 1:
             return hash_list[0]
+        if len(hash_list) % 2 != 0:
+            hash_list.append(hash_list[-1])  # 确保每次递归调用时，列表长度为偶数
         new_hash_list = []
         for i in range(0, len(hash_list), 2):
             new_hash = hashlib.sha256((hash_list[i] + hash_list[i+1]).encode()).hexdigest()
--- leader.py
+++ leader.py
@@ -2,6 +2,7 @@ import threading
 import socket
 import json
 import time
+from threading import Lock
 from blockchain.transaction import Transaction
 from blockchain.block import Block, Blockchain
 from ecdsa import SigningKey, NIST384p
@@ -18,7 +19,7 @@ def check_sign_header(a1,a2):
 
 class Node:
     def __init__(self, node_id, is_faulty=False):
-        self.sender_sk = SigningKey()
+        self.sender_sk = SigningKey.generate(curve=NIST384p)
         self.sender_vk = self.sender_sk.verifying_key
         self.node_id = node_id
         self.is_faulty = is_faulty
@@ -29,7 +30,7 @@ class Node:
         self.prepare_count = 0
         self.commit_count = 0
     def send_message(self, message, nodes):
-        for node in nodes:
+        for node in self.nodes:
             node.receive_message(message)
         print(f"Node {self.node_id}: Sent message {message}")
 
@@ -51,7 +52,7 @@ class Node:
         # block_data = json.dumps(block.to_dict()).encode('utf-8')  # 使用to_dict方法
         
         hash_block = block_md5(block)
-        header_dict = {"header":header,"id":id,"hash_block":hash_block,"pubkey":self.sender_vk.to_string().to_hex()}
+        header_dict = {"header":header,"id":id,"hash_block":hash_block,"pubkey":self.sender_vk.to_string().hex()}
         signed_header = sign_header(header_dict,self.sender_sk)
         
         data_dict = {"block_dict":block.to_dict(),"signed_header":signed_header,"header_dict" : header_dict}
@@ -66,6 +67,7 @@ class Node:
         except Exception as e:
             print(f"Error sending block to {node}:{port}: {e}")
     def handle_client_connection(self,client_socket, address):
+        # server_socket.bind(('0.0.0.0', self.port))
         try:
             msg_data = client_socket.recv(1024*1024*1024) 
             data_dict = json.loads(msg_data.decode("utf-8"))
@@ -74,14 +76,15 @@ class Node:
         except Exception as e:
             print(f"Error receving header")
             return
-        print(f"Received message from {address}: {message}")
+        print(f"Received message from {address}: {header}")
         if header.startswith("REQUEST"):
             if not check_sign_header(data_dict["header_dict"],data_dict["signed_header"]):
                 print(f"find sign error in the transaction")
             else:
-                if not self.blockchain.add_transaction(data_dict["block_dict"]) :
+                new_transaction = create_transaction_from_data(data_dict["block_dict"])
+                if not self.blockchain.add_transaction(new_transaction) :
                     print(f"error in transcation")
-                elif len(blockchain.pending_transactions) == 5 :
+                elif len(self.blockchain.pending_transactions) == 5 :
                     with self.blockchain.lock:
                         self.blockchain.pending_transactions.append(Transaction(None, self.node_id, self.blockchain.mining_reward))
                         block = Block(len(self.blockchain.chain), self.blockchain.pending_transactions, time.time(), self.blockchain.get_last_block().hash)
@@ -90,7 +93,7 @@ class Node:
                     header = "PRE-PREPARE"
                     block = self.blockchain.get_last_block()
                     threads = []
-                    for node in nodes:
+                    for node in self.nodes:
                         now_thread = threading.Thread(target=send_block_to_node, args=(self,header,node_id,node,5000,block))
                         threads.append(now_thread)
                     for thread in threads:
@@ -99,9 +102,9 @@ class Node:
                     with self.lock:
                         self.prepare_count = 1
                     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                    server_socket.bind(('0.0.0.0', port))
+                    server_socket.bind(('0.0.0.0', 7778))
                     server_socket.listen(5)
-                    print(f"Listening on port {port}...")
+                    print(f"Listening on port {7778}...")
 
 
                     for node in self.nodes:
@@ -116,7 +119,7 @@ class Node:
 
                     header = "COMMIT"
                     threads = []
-                    for node in nodes:
+                    for node in self.nodes:
                         now_thread = threading.Thread(target=send_block_to_node, args=(self,header,node_id,node,5000,block))
                         threads.append(now_thread)
                     for thread in threads:
@@ -125,9 +128,9 @@ class Node:
                     with self.lock:
                         self.commit_count = 1
                     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                    server_socket.bind(('0.0.0.0', port))
+                    server_socket.bind(('0.0.0.0', 7779))
                     server_socket.listen(5)
-                    print(f"Listening on port {port}...")
+                    print(f"Listening on port {7779}...")
 
 
                     for node in self.nodes:
@@ -144,7 +147,7 @@ class Node:
             else:
                 if not self.blockchain.add_transaction(data_dict["block_dict"]) :
                     print(f"error in transcation")
-                elif len(blockchain.pending_transactions) == 5 :
+                elif len(self.blockchain.pending_transactions) == 5 :
                     with self.blockchain.lock:
                         self.blockchain.pending_transactions.append(Transaction(None, self.node_id, self.blockchain.mining_reward))
                         block = Block(len(self.blockchain.chain), self.blockchain.pending_transactions, time.time(), self.blockchain.get_last_block().hash)
@@ -153,7 +156,7 @@ class Node:
                     header = "PREPARE"
                     block = self.blockchain.get_last_block()
                     threads = []
-                    for node in nodes:
+                    for node in self.nodes:
                         now_thread = threading.Thread(target=send_block_to_node, args=(self,header,node_id,node,5000,block))
                         threads.append(now_thread)
                     for thread in threads:
@@ -162,9 +165,9 @@ class Node:
                     with self.lock:
                         self.prepare_count = 2
                     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                    server_socket.bind(('0.0.0.0', port))
+                    server_socket.bind(('0.0.0.0', 7778))
                     server_socket.listen(5)
-                    print(f"Listening on port {port}...")
+                    print(f"Listening on port {7778}...")
 
                     for node in self.nodes:
                         with self.lock:
@@ -177,7 +180,7 @@ class Node:
 
                     header = "COMMIT"
                     threads = []
-                    for node in nodes:
+                    for node in self.nodes:
                         now_thread = threading.Thread(target=send_block_to_node, args=(self,header,node_id,node,5000,block))
                         threads.append(now_thread)
                     for thread in threads:
@@ -186,9 +189,9 @@ class Node:
                     with self.lock:
                         self.commit_count = 1
                     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                    server_socket.bind(('0.0.0.0', port))
+                    server_socket.bind(('0.0.0.0', 7779))
                     server_socket.listen(5)
-                    print(f"Listening on port {port}...")
+                    print(f"Listening on port {7779}...")
 
                     for node in self.nodes:
                         with self.lock:
@@ -251,7 +254,7 @@ def leader_server(node,port):
             client_sock, address = server_socket.accept()
             print(f"Accepted connection from {address}")
             client_handler = threading.Thread(
-                target=handle_client_connection,
+                target=node.handle_client_connection,
                 args=(client_sock, address)
             )
             client_handler.start()
@@ -259,7 +262,7 @@ def leader_server(node,port):
         server_socket.close()
 
 if __name__ == '__main__':
-    node = Node()
+    node = Node(1, False, 7778)
     leader_server(node,7777)
 
 # nodes = [PBFTNode(i) for i in range(4)]
@@ -268,11 +271,11 @@ if __name__ == '__main__':
 # primary_node = nodes[0]
 # primary_node.pre_prepare("transaction1", nodes)
 
-# for node in nodes:
+# for node in self.nodes:
 #     node.prepare("transaction1", nodes)
 #     node.commit("transaction1", nodes)
 
-# for node in nodes:
+# for node in self.nodes:
 #     if hasattr(node, 'committed') and node.committed:
 #         node.execute("transaction1")
 
@@ -283,11 +286,11 @@ if __name__ == '__main__':
 #     primary_node = nodes[0]
 #     primary_node.pre_prepare("test_transaction", nodes)
 
-#     for node in nodes:
+#     for node in self.nodes:
 #         node.prepare("test_transaction", nodes)
 #         node.commit("test_transaction", nodes)
 
-#     for node in nodes:
+#     for node in self.nodes:
 #         if hasattr(node, 'committed') and node.committed:
 #             node.execute("test_transaction")
 
